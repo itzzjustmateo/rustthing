bbb65027ec89c2d5d72f8bcb93481d3940941129
ItzzMateo <itzzmateo@devflare.de>
2026-07-16 19:07:52 +0200

    feat(game): finish guessing

---
+++ src/guessing.rs
@@ -0,0 +1,40 @@
+use std::cmp::Ordering; // 1. Must import this for the match to work!
+use std::io::{self, Write};
+use std::{thread, time};
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::random_range(1..=100);
+
+    loop {
+        print!("Please input your guess: ");
+        io::stdout().flush().expect("Failed to flush stdout");
+
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                thread::sleep(time::Duration::from_millis(450));
+                println!("Please input a number");
+                thread::sleep(time::Duration::from_millis(450));
+                continue;
+            }
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You won!");
+                break;
+            }
+        }
+    }
+}
